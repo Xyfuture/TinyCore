dc2e8bcbdc2e7fb9368de1011e4c839c98d78ed7
Xyfuture <1728027202@qq.com>
2022-04-07 19:42:23 +0800

    add binary code file read

--- src/main/scala/stage/IF.scala
+++ src/main/scala/stage/IF.scala
@@ -1,5 +1,8 @@
 package stage
 
+import scala.io.Source
+import scala.math
+
 import spinal.core._
 import spinal.lib._
 import connection.{ CtrlSignalMaster, IF_ID}
@@ -7,20 +10,36 @@ import base.ISA._
 
 import scala.language.postfixOps
 
-class IF extends Component{
+class IF(instFileName:String="None") extends Component{
   val io = new Bundle{
     val output = master Flow new IF_ID
 //    val ctrl = CtrlSignalMaster()
   }
 
-  val pc_reg = Reg(UInt(64 bits))
+  val pcReg = Reg(UInt(64 bits)) init(0)
 
 
-  pc_reg := pc_reg + 1
+  pcReg := pcReg + 1
 
+  val programString = getFileString(instFileName)
+  val addrLen = scala.math.ceil(scala.math.log(programString.length)/scala.math.log(2)).toInt
+  val convertBits = (strArray:Array[String]) => for (ele <- strArray) yield B(ele.toInt, INSTRUCTION_WORD_LEN bits)
 
-  val inst_buffer = Mem(Bits(INSTRUCTION_WORD_LEN bits), Array[Bits]( B(0,32 bits),B(1,32 bits),B(0,32 bits),B(1,32 bits)))
+  val instBuffer = Mem(Bits(INSTRUCTION_WORD_LEN bits),convertBits(programString))
 
-  io.output.payload.instruction := inst_buffer.readSync(address = pc_reg(1 downto 0))
+  io.output.payload.instruction := instBuffer.readSync(address = pcReg(addrLen-1 downto 0))
   io.output.valid := True
+
+  def getFileString(fileName:String) = {
+    if (fileName != "None"){
+      val source = Source.fromFile(fileName,"UTF-8")
+      val strArray = source.getLines().toArray
+//      val programData = for (elem <- strArray) yield B(elem.toInt,INSTRUCTION_WORD_LEN bits)
+//      programData
+      strArray
+    }
+    else {
+      Array[String] ("312","2313","543")
+    }
+  }
 }
